838df2d499c19e2caabe7a32e3f640a78afd5c64
if23b265 <if23b265@technikum-wien.at>
2026-02-02 15:11:46 +0100

    Create ThreadPool class header and define constructor in .cpp

--- src/ThreadPool.cpp
+++ src/ThreadPool.cpp
@@ -1,5 +1,34 @@
 #include "ThreadPool.h"
 
-ThreadPool::ThreadPool() {
-    
+ThreadPool::ThreadPool(size_t numThreads) : stop(false) {
+    for (size_t i = 0; i < numThreads; ++i) {
+        // Wir erstellen jetzt numThreads Threads, rufen dafür emplace_back auf, welcher eine Lambda Funktion aufruft, welche auf alle Member der ThreadPool-Klasse zugreifen soll ([this])
+        workers.emplace_back([this]() {
+            // Jeder Threat läuft endlos in der Schleife bis er explizit beendet wird
+            while (true) {
+                // Momentane Aufgabe deklariert
+                std::function<void()> task;
+                // Block-Scope durch {}
+                {
+                    std::unique_lock<std::mutex> lock(queueMutex);
+
+                    // Thread schläft bis stop == true ODER tasks nicht leer
+                    condition.wait(lock, [this]() {
+                        return stop || !tasks.empty();
+                    });
+
+                    // Thread beendet sich wenn stop UND tasks.empty() true sind
+                    if (stop && tasks.empty()) {
+                        return;
+                    }
+
+                    // Nimmt nächste Aufgabe aus Queue
+                    task = std::move(tasks.front());
+                    tasks.pop();
+                }
+
+                task();
+            }
+        });
+    }
 }
--- src/ThreadPool.h
+++ src/ThreadPool.h
@@ -1,11 +1,26 @@
 #ifndef THREADPOOL_H
 #define THREADPOOL_H
 
+#include <functional>
+#include <thread>
+#include <queue>
+#include <mutex>
+#include <condition_variable>
+
 class ThreadPool {
     public:
+        ThreadPool(size_t numThreads);
+        ~ThreadPool();
+
+        void enqueue(std::function<void()> task);
 
     private:
+        std::vector<std::thread> workers;           // Workers
+        std::queue<std::function<void()>> tasks;    // Task Warteschlange
 
+        std::mutex queueMutex;                      // schützt die Queue
+        std::condition_variable condition;          // weckt schlafende Threads
+        bool stop;
 };
 
 #endif
