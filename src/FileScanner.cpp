5b55d6bd123dd19aaebc45d67c8d5e11e210707c
if23b265 <if23b265@technikum-wien.at>
2026-02-03 14:17:38 +0100

    Protect std::cout in scanDirectory with mutex for thread-safe logging

--- src/FileScanner.cpp
+++ src/FileScanner.cpp
@@ -8,8 +8,14 @@
 namespace fs = std::filesystem;
 
 void scanDirectory(const std::string& path, ThreadPool& pool) {
-    std::cout << "Scanning folder: " << path << " | Thread: " << std::this_thread::get_id() << "\n";
 
+    static std::mutex coutMutex;
+
+    {
+        std::lock_guard<std::mutex> lock(coutMutex);
+        std::cout << "Scanning folder: " << path << " | Thread: " << std::this_thread::get_id() << "\n";
+    }
+    
     for (const auto& entry : fs::directory_iterator(path)) {
 
         if (entry.is_directory()) {
@@ -22,7 +28,10 @@ void scanDirectory(const std::string& path, ThreadPool& pool) {
         }
 
         else if (entry.is_regular_file()) {
-            std::cout << "Found file: " << entry.path() << " | Thread: " << std::this_thread::get_id() << "\n";
+            {
+                std::lock_guard<std::mutex> lock(coutMutex);
+                std::cout << "Found file: " << entry.path() << " | Thread: " << std::this_thread::get_id() << "\n";
+            }
         }
     }
 }
